ac2293346a4b90c513d48ca58d1ec979164c8695
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-10-28 15:57:40 +0200

    adding necessary objects to run JavaFX

--- src/main/java/com/tlj/JavaFxLaunchpad.java
+++ src/main/java/com/tlj/JavaFxLaunchpad.java
@@ -1,12 +1,20 @@
 package com.tlj;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class JavaFxLaunchpad extends Application {
 
     @Override
     public void start(Stage stage) {
+        JavaFxSceneBuilder javaFxSceneBuilder = new JavaFxSceneBuilder();
+        Scene scene = new Scene(javaFxSceneBuilder, 500, 500);
+        stage.setScene(scene);
+        stage.show();
+    }
 
+    public static void main(String[] args) {
+        launch();
     }
 }
---
+++ src/main/java/com/tlj/JavaFxSceneBuilder.java
@@ -0,0 +1,6 @@
+package com.tlj;
+
+import javafx.scene.layout.VBox;
+
+public class JavaFxSceneBuilder extends VBox {
+}
