a4f7339b3a622a3f31db57babac80024033d4469
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-10-28 15:51:48 +0200

    added class to launch JavaFx from and started adding necessary methods to set it up

---
+++ src/main/java/com/tlj/JavaFxLaunchpad.java
@@ -0,0 +1,12 @@
+package com.tlj;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFxLaunchpad extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+    }
+}
