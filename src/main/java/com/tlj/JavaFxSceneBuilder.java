9e29521dcd3f63af8a01558645044e28bb7b68f4
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-10-28 16:01:03 +0200

    adding canvas on which to draw board as well as creating private instance of GameBoard from which to connect to back end methods

--- src/main/java/com/tlj/JavaFxSceneBuilder.java
+++ src/main/java/com/tlj/JavaFxSceneBuilder.java
@@ -3,8 +3,14 @@ package com.tlj;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
+import java.awt.*;
+
 public class JavaFxSceneBuilder extends VBox {
 
     private Button evolveButton;
     private Button clearButton;
+
+    private Canvas canvas;
+
+    private GameBoard gameBoard;
 }
