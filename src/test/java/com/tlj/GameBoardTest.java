35bcbc0d3ccf5e7cd94a25b048256145bb4576e1
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-10-28 14:47:30 +0200

    added test to check cell dies from overpopulation if > 3 living neighbours

--- src/test/java/com/tlj/GameBoardTest.java
+++ src/test/java/com/tlj/GameBoardTest.java
@@ -109,4 +109,19 @@ public class GameBoardTest {
         gameBoard.evolve();
         assertEquals(0,gameBoard.get_cell_state(2,2));
     }
+
+    @Test
+    public void livingCellWithMoreThan3LivingNeighboursShouldDieFromOverpopulation() {
+        gameBoard.set_cell_state_to_dead(1,1);
+        gameBoard.set_cell_state_to_alive(1,2);
+        gameBoard.set_cell_state_to_alive(1,3);
+        gameBoard.set_cell_state_to_alive(2,1);
+        gameBoard.set_cell_state_to_alive(2,2);
+        gameBoard.set_cell_state_to_alive(2,3);
+        gameBoard.set_cell_state_to_dead(3,1);
+        gameBoard.set_cell_state_to_dead(3,2);
+        gameBoard.set_cell_state_to_dead(3,3);
+        gameBoard.evolve();
+        assertEquals(0,gameBoard.get_cell_state(2,2));
+    }
 }
