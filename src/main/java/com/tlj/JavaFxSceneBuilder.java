7940bfd0ad92608cce04071d2ca112c6da278c71
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-10-28 15:59:39 +0200

    import Button from JavaFX and initialising for calling evolve and clear_board methods in back end

--- src/main/java/com/tlj/JavaFxSceneBuilder.java
+++ src/main/java/com/tlj/JavaFxSceneBuilder.java
@@ -1,6 +1,10 @@
 package com.tlj;
 
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 public class JavaFxSceneBuilder extends VBox {
+
+    private Button evolveButton;
+    private Button clearButton;
 }
