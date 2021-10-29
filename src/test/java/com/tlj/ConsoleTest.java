a0fb6ff5b1a21f2423e364f7c99eca3bacf7975e
Tom Lloyd-Jones <67908875+tljbwdwor@users.noreply.github.com>
2021-10-29 12:41:15 +0200

    added failing test to get x axis from user and created gameBoard instance of size 100 x 100 which will be the max size permitted in the console going forwards

--- src/test/java/com/tlj/ConsoleTest.java
+++ src/test/java/com/tlj/ConsoleTest.java
@@ -1,6 +1,14 @@
 package com.tlj;
 
-class ConsoleTest {
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class ConsoleTest {
 
+    GameBoard gameBoard = new GameBoard(100,100);
+
+    @Test
+    void shouldRetrieveXAxisValueFromUser() {
+        assertEquals(15,get_x_axis_from_user());
+    }
 }
